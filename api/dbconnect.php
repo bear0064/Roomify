ad7ee0bf8c0271a91bb791bb96b9030be2c39869
m4ttyng <youn0482@algonquinlive.com>
2016-04-20 15:56:08 -0400

    added some commenting
    Client instructions for changing dbconnect info

--- api/dbconnect.php
+++ api/dbconnect.php
@@ -1,8 +1,17 @@
 <?php
+
+//1. Change $dbhost's value to the web hosts url.
+//2. Change $dbname's value to the database name
+//   in the PHP My Admin page (default is 'ten23mb')
+//3. Change the $dbuser and $dbpass value to the
+//   login credentials provided by web host
+
 $dbhost = 'localhost';
 $dbname = 'ten23mb';
 $dbuser = 'root';
 $dbpass = 'root';
+
+
 try {
     $conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
